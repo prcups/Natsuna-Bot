0331a0c01e99af5e2854d00e63fd8fa59ff30a5d
Yuno <prcups@163.com>
2021-08-29 17:38:07 +0800

    modified

--- cqhandler/cqhandler.go
+++ cqhandler/cqhandler.go
@@ -1,57 +1,30 @@
 package cqhandler
 
 import (
-	"log"
+	"encoding/json"
 	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
-	"encoding/json"
-	"net/http"
-	"errors"
-	_ "plugin"
-	_ "filepath"
-	"math/rand"
+	"strconv"
 )
 
-type naddr {
-	qqnum string
-	addr string
-	to int
-	isSent bool
-}
-
-var aa [105]naddr
-var l int
 
 const (
 	HTTP_ADDRESS = "http://127.0.0.1:5700"
 	WEBSOCKET_ADDRESS = "ws://127.0.0.1:6700"
 )
 
-func commandHandler(resStr []byte) {
-		err = filepath.Walk("../recv_plugins", func(path string, info fs.FileInfo, err error) error {
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-			filepath.
-		})
-		
-		if resMap["message_type"] == "private" {
-			
-		} else if resMap["message_type"] == "group" {
-			
-		}
-	}
- }
 
 func BootHandler() {
-	url, err := url.Parse("ws://localhost:6700")
+	wsurl, err := url.Parse("ws://localhost:6700")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	
-	ws, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	ws, _, err := websocket.DefaultDialer.Dial(wsurl.String(), nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -66,56 +39,33 @@ func BootHandler() {
 			log.Println("interrupt")
 			err := ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
-				log.Fatal(err.Error)
+				log.Fatal(err)
 			}
 			return
 		default: 
-			id, resStr, err := ws.ReadMessage()
+			_, resStr, err := ws.ReadMessage()
 			if err != nil {
 				log.Fatal(err)
 			}
-			commandHandler(message)
-			var resMap, senderMap map
-			err := json.Unmarshal(resStr, &resMap)
+			var resMap map[string]interface{}
+			err = json.Unmarshal(resStr, &resMap)
 			if err != nil {
-				log.Println(err.Error())
+				log.Println(err)
 			}
-			
-			if resMap["message_type"] == "private" && resMap["message"][0] == ":" {
-				l++
-				aa[l] = {
-					qqnum: resMap["user_id"],
-					addr: resMap["message"][1:],
-					to: l,
-					isSent: false
+
+			log.Println(resMap)
+			if resMap["notice_type"] == "group_increase" && int(resMap["group_id"].(float64)) == 247736999 {
+				log.Println("来客人了")
+				_, err := http.PostForm(
+					HTTP_ADDRESS + "/send_group_msg",
+					url.Values{
+						"group_id": {"247736999"},
+						"message": {"[CQ:at,qq=" + strconv.FormatInt(int64(resMap["user_id"].(float64)), 10) + "] 欢迎热爱技术的你加入兰州大学开源社区的大家庭！想了解更多请阅读群公告新生指南噢！"},
+					},
+				)
+				if err != nil {
+					log.Println(err)
 				}
-				ws.WriteMessage(id, "{ reply:/"记录成功！/"")
-				//resp, err := http.PostForm({
-					//"http://127.0.0.1:5700/send_private_msg",
-					//url.Values{"user_id": resMap[user_id], "message": {"添加记录成功！"}}
-				//})
-				//resp.Body.Close()
-			}	else if resMap["message"] == "分发地址" {
-				for i := 1; i <= l; i++ {
-					if aa[i].to == i {
-						j := rand.Int() % l + 1
-						for aa[i].to == j || aa[j].to == i {
-							j := rand.Int() % l + 1
-						}
-						t := aa[i].to
-						aa[i].to = aa[j].to
-						aa[j].to = t
-					}
-				}
-				
-				for i := 1; i <= l; i++ {
-					resp, _ := http.PostForm({
-						"http://127.0.0.1:5700/send_private_msg",
-						url.Values{"user_id": resMap[user_id], "group_id": "1022606672", message": aa[aa[i].to].addr}
-					})
-					resp.Body.Close()
-				}
-				l = 0
 			}
 		}
 	}
